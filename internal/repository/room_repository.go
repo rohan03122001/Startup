62f8a062272a53c277b4429dc73883219d59566f
Rohan Bhujbal <rxb421@student.bham.ac.uk>
2025-01-02 18:47:29 +0000

    repository layer done

--- internal/repository/room_repository.go
+++ internal/repository/room_repository.go
@@ -24,3 +24,25 @@ func(r *RoomRepository) GetRoomByCode(code int) (*models.Room, error){
 	return &room, nil
 }
 
+//Get Active Rooms
+
+func(r *RoomRepository) GetActive() ([]models.Room, error){
+	var rooms []models.Room
+
+	if err:=r.db.Where("status = ?","waiting").Or("status = ?", "playing").Find(&rooms).Error; err!=nil{
+		return nil, err
+	}
+
+	return rooms, nil
+}
+
+//update Room Status
+
+func(r *RoomRepository) UpdateStatus(RoomID string, status string) error{
+	return r.db.Model(&models.Room{}).Where("id=?",RoomID).Update("status", status).Error
+}
+
+// Delete removes a room
+func (r *RoomRepository) Delete(id string) error {
+    return r.db.Delete(&models.Room{}, "id = ?", id).Error
+}
